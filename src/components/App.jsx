cfc4b7c70bd2cfe7c00c4d24853bc92485730120
DenysVM <muratov.d@gmail.com>
2023-09-07 12:50:34 +0200

    Книга контактів. Крок 3.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,14 +5,20 @@ import { nanoid } from 'nanoid';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
     name: '',
     number: '',
   };
 
   addContact = (name, number) => {
     const contact = {
-      id: nanoid(), 
+      id: nanoid(),
       name,
       number,
     };
@@ -22,13 +28,30 @@ class App extends Component {
     }));
   };
 
+  handleFilterChange = (e) => {
+    this.setState({ filter: e.target.value.toLowerCase() });
+  };
+
   render() {
+    const { contacts, filter } = this.state;
+    const filteredContacts = contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter)
+    );
+
     return (
       <div>
         <h1>Phonebook</h1>
         <ContactForm addContact={this.addContact} />
         <h2>Contacts</h2>
-        <ContactList contacts={this.state.contacts} />
+        <label>
+          Find contacts by name:
+          <input
+            type="text"
+            value={filter}
+            onChange={this.handleFilterChange}
+          />
+        </label>
+        <ContactList contacts={filteredContacts} />
       </div>
     );
   }
